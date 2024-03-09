48a312539f2fda392ef82d63d34a3440dfbefae3
김형준 <gudwns051613@gmail.com>
2024-03-09 20:18:51 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,39 +1,56 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-// The main method must be in a class named "Main".
-class Main {
-    static int N;
-    static boolean[] visited;
+public class Main {
     static int[][] network;
+    static boolean[] visited;
+    static int com;
+    static int con;
     static int count = 0;
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        visited = new boolean[N+1];
-        network = new int[N+1][N+1];
-        int M = Integer.parseInt(br.readLine());
-        for(int i = 0; i < M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            network[n][m] = 1;
-            network[m][n] = 1;
-        }
-        dfs(1);
 
-        System.out.println(count-1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        com = Integer.parseInt(br.readLine());
+        con = Integer.parseInt(br.readLine());
+
+        network = new int[com+1][com+1];
+        visited = new boolean[com+1];
+
+        StringTokenizer st;
+        for (int i = 0; i < con; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            network[a][b] = 1;
+            network[b][a] = 1;
+        }
+
+        bfs(1);
+        System.out.println(count);
     }
 
-    public static void dfs(int a){
-        visited[a] = true;
-        count++;
-        for(int i = 1; i < N + 1; i++){
-            if(!visited[i] && network[a][i] == 1){
-                dfs(i);
+    static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int n = queue.poll();
+
+            for (int i = 1; i < com + 1; i++) {
+                if (network[n][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                    count++;
+                }
             }
         }
+
+
     }
 }
