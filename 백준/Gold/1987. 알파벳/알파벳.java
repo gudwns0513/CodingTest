c64416abe0b4eb7b2631104356913f200586106a
김형준 <gudwns051613@gmail.com>
2024-03-14 17:46:58 +0900

    [Gold IV] Title: 알파벳, Time: 896 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -17,11 +17,6 @@ public class Main {
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        
-        if (R == 1 && C == 1) {
-            System.out.println(1);
-            return;
-        }
 
         board = new char[R][C];
         visited = new boolean[26]; //알파벳의 개수
@@ -33,32 +28,27 @@ public class Main {
             }
         }
 
-        dfs(0, 0, 0);
+        dfs(0, 0, 1);
         System.out.println(ans);
 
     }
 
     static void dfs(int x, int y, int count) {
+        ans = Math.max(ans, count);
+        visited[board[x][y] - 65] = true;
 
-        if (visited[board[x][y] - 65]) {
-            ans = Math.max(ans, count);
-            return;
-        }
-        else {
-            visited[board[x][y] - 65] = true;
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
+            if (newX < 0 || newX >= R || newY < 0 || newY >= C) { //상하좌우를 모두 돌 때 board의 크기를 벗어나면 dfs실행하지않음
+                continue;
+            }
 
-                if (newX < 0 || newX >= R || newY < 0 || newY >= C) { //상하좌우를 모두 돌 때 board의 크기를 벗어나면 dfs실행하지않음
-                    continue;
-                }
-
+            if(!visited[board[newX][newY] - 65]){
                 dfs(newX, newY, count + 1);
+                visited[board[newX][newY] - 'A'] = false; //65 대신 'A'써도 무방
             }
         }
-
-        visited[board[x][y] - 'A'] = false; //65를 빼도 무방 --> 이거 잘 모르겠음
     }
 }
