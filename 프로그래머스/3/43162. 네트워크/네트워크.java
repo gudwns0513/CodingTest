1753c0ceed250df08998c076dda14cc3bbb9bd1e
김형준 <gudwns051613@gmail.com>
2024-08-24 10:36:46 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,32 @@
+class Solution {
+    int answer = 0;
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                answer++;
+                dfs(i, computers);
+            }
+                
+        }
+        return answer;
+    }
+    
+    public void dfs(int network, int[][] computers){
+        if(visited[network]){ //재귀함수 조건문
+           return;
+        }
+        
+        visited[network] = true; //방문 기록
+        
+        for(int i = 0; i < computers.length; i++){//조건 만족하는 연결 네트워크 모두 방문
+            if(i != network && computers[network][i] == 1){
+                dfs(i, computers);
+            }
+        }
+        
+        
+    }
+}
